a187b85c17dee92040625879ce90e792af153a78
jaslynpalomino <40184474+jaslynpalomino@users.noreply.github.com>
2018-06-20 11:53:12 -0700

    Add files via upload

--- socialnetwork.py
+++ socialnetwork.py
@@ -1,10 +1,14 @@
-## Made by: Jaslyn Palomino
+## Jaslyn Palomino
 ## My Social Network
 
 
-## Adding a friend with only using a username
-        
+    ## Adding a friend with only using a username
+    
 class User:
+
+    def createID(self, num):
+        self.userID = num
+    
     def __init__(self, username):
             self.username = username
             self.firstName = ""
@@ -12,7 +16,18 @@ class User:
             self.bio = ""
             self.friendslist = []
             self.posts = []
-            
+            friendsList = []
+            posts = []
+
+    def createPost(self, Content):
+        mypost = post(content)
+        self.post.append(mypost)
+        myPost.createPostID(len(post))
+        self.userID = ""
+
+    def createUserID(self, num):
+        self.userID = num
+    
     def addfirstName(self, firstName):
         self.firstName = firstName
 
@@ -41,33 +56,56 @@ class User:
     def showUsername (self):
         for friend in self.friendslist:
             print (friend.username)
- 
+
+class Post:
+    def __init__(self, content):
+        self.content = content
+        self.postID = len
+        self.commments = []
+
+    def createPostID(self, num):
+        self.postID = num
+
+class Network:
+    def __init__ (self):
+        self.users = []
+        print ("User Created")
+
+    def createUser(self, username):
+        for user in self.users:
+            if (user.username == username):
+                print ("Username Taken")
+        myUser = User(username)
+        self.users.append(myUser)
+        mySize = len(self.users)
+        myUser.createUserID(mySize)
+        
 if __name__ == "__main__":
+    network = Network ()
+    network.createUser("jaslynpolynomial")
                   
     username = "jaslynpolynomial"
                   
-    jaslyn = User("jaslynpolynomial")
+    jaslyn = User("jaslynpolynomial") 
     em = User("em")
     maxuel = User ("maxuel")
 
-##jaslyn.addpost("hi")
-##jaslyn.unfriend("maxuel")
+                                    ##jaslyn.addpost("hi")
+                                    ##jaslyn.unfriend("maxuel")
 
-
-                  
-    ##My Account
+##My Account
     firstName = "Jaslyn"
     lastName = "Palomino"
     username = "jaslynpolynomial"
     bio = "Hi, my name last name is actually Palomino"
     userID = "0000"
 
-    ##User
+##User
     jaslyn = User("jaslynpolynomial")  
     maxx = User("maxuel")
     emily = User("em")
     
-    ##Posts
+##Posts
     emily.addposts("Hello")
     
     
