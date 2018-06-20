b10d9139a06a550dcffe2f128de971237789d804
jaslynpalomino <40184474+jaslynpalomino@users.noreply.github.com>
2018-06-20 14:52:13 -0700

    Add files via upload

--- socialnetwork.py
+++ socialnetwork.py
@@ -1,7 +1,6 @@
 ## Jaslyn Palomino
 ## My Social Network
 
-
     ## Adding a friend with only using a username
     
 class User:
@@ -32,7 +31,7 @@ class User:
         self.firstName = firstName
 
     def addlastName (self, lastName):
-        self.lastName = lastName
+          self.lastName = lastName
 
     def addbio(self, bio):
         self.bio = bio
@@ -44,6 +43,7 @@ class User:
         for friend in self.friendslist:
             print(friend.username)
             print(friend.posts)
+                                                                                                    ##kms
     
     def unFriend(self, obj):
         for friend in self.friendslist:
@@ -62,9 +62,12 @@ class Post:
         self.content = content
         self.postID = len
         self.commments = []
-
+ 
     def createPostID(self, num):
         self.postID = num
+
+##    def addComment(self, comment):
+##        self.comment.append.(comment)
 
 class Network:
     def __init__ (self):
@@ -72,52 +75,70 @@ class Network:
         print ("User Created")
 
     def createUser(self, username):
-        for user in self.users:
-            if (user.username == username):
-                print ("Username Taken")
-        myUser = User(username)
+##        for user in self.users:
+##            if (user.username == username):
+##                print ("Username Taken")
+        myUser = User(username, mySize)
         self.users.append(myUser)
-        mySize = len(self.users)
-        myUser.createUserID(mySize)
+        mySize = int(len(self.users))+1
+##        myUser.createUserID(mySize)
+        print(len(self.users))
+
+    def addConnection(self, user1, user2):
+        user1OBJ = self.getOBJ(user1)
+        user2OBJ = self.getOBJ(user2)
+
+        user1OBJ.addfriends(user2OBJ)
+        user2OBJ.addfriends(user1OBJ)
+
+    def getOBJ(self, username):
+        userID = self.getUserID(username)
+        userOBJ = self.user [userID-1]
+        print(userOBJ.username)
+        return userOBJ
+
+    def getsUserID(self, username):
+        for i in self.users:
+            if i.username == username:
+                return i.userID
+        
         
 if __name__ == "__main__":
     network = Network ()
     network.createUser("jaslynpolynomial")
                   
-    username = "jaslynpolynomial"
-                  
-    jaslyn = User("jaslynpolynomial") 
-    em = User("em")
-    maxuel = User ("maxuel")
-
-                                    ##jaslyn.addpost("hi")
-                                    ##jaslyn.unfriend("maxuel")
-
-##My Account
-    firstName = "Jaslyn"
-    lastName = "Palomino"
-    username = "jaslynpolynomial"
-    bio = "Hi, my name last name is actually Palomino"
-    userID = "0000"
-
-##User
-    jaslyn = User("jaslynpolynomial")  
-    maxx = User("maxuel")
-    emily = User("em")
-    
-##Posts
-    emily.addposts("Hello")
-    
-    
-    jaslyn.addFriend(maxx)
-    jaslyn.addFriend(emily)
-
-    jaslyn.viewFeed()
-
-    jaslyn.showUsername()
-
-    jaslyn.unFriend(maxx)
-
-    jaslyn.showUsername()
-   
-
+##    username = "jaslynpolynomial"
+##                  
+##    jaslyn = User("jaslynpolynomial") 
+##    em = User("em")
+##    maxuel = User ("maxuel")
+##
+##                                    ##jaslyn.addpost("hi")
+##                                    ##jaslyn.unfriend("maxuel")
+####My Account
+##    firstName = "Jaslyn"
+##    lastName = "Palomino"
+##    username = "jaslynpolynomial"
+##    bio = "Hi, my name last name is actually Palomino"
+##    userID = "0000"
+##
+####User
+##    jaslyn = User("jaslynpolynomial")  
+##    maxx = User("maxuel")
+##    emily = User("em")
+##    
+####Posts
+##    emily.addposts("Hello world")
+##    maxx.addposts("This is a new post by Maxx")
+##    
+##    jaslyn.addFriend(maxx)
+##    jaslyn.addFriend(emily)
+##
+##    jaslyn.viewFeed()
+##
+##    jaslyn.showUsername()
+##
+##    jaslyn.unFriend(maxx)
+##
+##    jaslyn.showUsername()
+ 
